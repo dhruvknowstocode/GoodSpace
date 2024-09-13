5023577fad445e61e8f1349bc3fdcaf55ca87f06
dhruv jain <dhruvjain2424@gmail.com>
2024-09-13 15:49:28 +0530

    added some of the styling in integrity sections

--- src/components/Integrations.jsx
+++ src/components/Integrations.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Integrations.scss';
 import flowchart from '../assets/vector.png'; // Add the flowchart image here
+import hrms from '../assets/hrms.png';
+import sso from '../assets/sso.png';
+import finance from '../assets/finance.png';
 
 const Integrations = () => {
   return (
@@ -11,7 +14,7 @@ const Integrations = () => {
       <div className="integration-cards">
         <div className="card hrms">
           <div className="icon"> {/* Icon Placeholder */}
-            <img src="/path/to/hrms-icon.png" alt="HRMS Icon" />
+            <img src={hrms} alt="HRMS Icon" />
           </div>
           <h3>HRMS Systems</h3>
           <p>
@@ -26,7 +29,7 @@ const Integrations = () => {
 
         <div className="card finance">
           <div className="icon"> {/* Icon Placeholder */}
-            <img src="/path/to/finance-icon.png" alt="Finance Icon" />
+            <img src={finance} alt="Finance Icon" />
           </div>
           <h3>Finance Systems</h3>
           <p>
@@ -38,7 +41,7 @@ const Integrations = () => {
 
         <div className="card sso">
           <div className="icon"> {/* Icon Placeholder */}
-            <img src="/path/to/sso-icon.png" alt="SSO Icon" />
+            <img src={sso} alt="SSO Icon" />
           </div>
           <h3>Single Sign-on Systems</h3>
           <p>

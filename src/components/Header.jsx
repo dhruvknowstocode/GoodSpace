f00744a838a512f913ed3d57f7b2eaa1e746fb58
dhruv jain <dhruvjain2424@gmail.com>
2024-09-14 12:41:01 +0530

    made header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../assets/logo.png';
 import "./Header.scss";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="container">
         <div className="logo">
           <img src={logo} alt="ExpenseBook Logo" />
         </div>
-        <nav className="nav">
+
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </button>
+
+        <nav className={`nav ${menuOpen ? "active" : ""}`}>
           <ul>
             <li><a href="#blog">Blogs</a></li>
             <li><a href="#features">Features</a></li>
@@ -17,6 +30,7 @@ const Header = () => {
             <li><a href="#integrations">Integrations</a></li>
           </ul>
         </nav>
+        
         <button className="btn-start-trial">Start Trial</button>
       </div>
     </header>
